86461f5869247db5a62cd234a44c2c769a7bc147
dongch007 <dongch007@gmail.com>
2018-11-27 20:54:58 +0800

    optimize ClampUint8
    decode 1000times
    3176ms->2315ms

--- TT/Math.h
+++ TT/Math.h
@@ -2,45 +2,49 @@
 #include "BaseType.h"
 namespace TT
 {
-	inline uint8 ClampUint8(int32 x)
+	inline uint8 ClampUint8(int32 n)
 	{
-		if (x < 0)
+		//if (n < 0)
+		//	return 0;
+
+		//if (n > 255)
+		//	return 255;
+
+		//return n;
+
+		//https://codereview.stackexchange.com/questions/6502/fastest-way-to-clamp-an-integer-to-the-range-0-255
+		n &= -(n >= 0);
+		return n | ((255 - n) >> 31);
+	}
+
+	inline uint8 ClampUint8Left(int32 n)
+	{
+		if (n < 0)
 			return 0;
 
-		if (x > 255)
+		return n;
+	}
+
+	inline uint8 ClampUint8Right(int32 n)
+	{
+		if (n > 255)
 			return 255;
 
-		return x;
+		return n;
 	}
 
-	inline uint8 ClampUint8Left(int32 x)
+	inline int32 Clamp(int32 n, int32 min, int32 max)
 	{
-		if (x < 0)
-			return 0;
+		if (n < min)
+			n = min;
+		if (n > max)
+			n = max;
 
-		return x;
+		return n;
 	}
 
-	inline uint8 ClampUint8Right(int32 x)
-	{
-		if (x > 255)
-			return 255;
-
-		return x;
-	}
-
-	inline int32 Clamp(int32 x, int32 min, int32 max)
-	{
-		if (x < min)
-			x = min;
-		if (x > max)
-			x = max;
-
-		return x;
-	}
-
-	inline int32 extend_4to8bits(int32 x) { return (x << 4) | x; }
-	inline int32 extend_5to8bits(int32 x) { return (x << 3) | (x >> 2); }
-	inline int32 extend_6to8bits(int32 x) { return (x << 2) | (x >> 4); }
-	inline int32 extend_7to8bits(int32 x) { return (x << 1) | (x >> 6); }
+	inline int32 extend_4to8bits(int32 n) { return (n << 4) | n; }
+	inline int32 extend_5to8bits(int32 n) { return (n << 3) | (n >> 2); }
+	inline int32 extend_6to8bits(int32 n) { return (n << 2) | (n >> 4); }
+	inline int32 extend_7to8bits(int32 n) { return (n << 1) | (n >> 6); }
 }
